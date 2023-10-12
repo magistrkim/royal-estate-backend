c2a4d50aa957d4cc4b5cbce42d6ff3a4b44a3d40
Nataliya Kim <magistr1982@gmail.com>
2023-10-12 08:33:43 +0300

    Connect to database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+// l1iV9rLIwysk5Fpc
+// mongodb+srv://magistrkim:<password>@royal-estate.pryvmxl.mongodb.net/
+// localhost:27017
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() => {
+    console.log("Successful connection to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
