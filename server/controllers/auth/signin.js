125896600e79ee45dd0a1adcacc92c6cef4024c0
Nataliya Kim <magistr1982@gmail.com>
2023-10-13 18:02:09 +0300

    Create signin api route

---
+++ server/controllers/auth/signin.js
@@ -0,0 +1,28 @@
+import User from "../../models/user.js";
+import { errorHandler } from "../../helpers/index.js";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword)
+      return next(errorHandler(401, "Wrong email or password"));
+      const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+      const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default signin;
--- server/helpers/index.js
+++ server/helpers/index.js
@@ -1,2 +1,3 @@
 export { default as HttpError } from "./HttpError.js";
 export { default as controllerWrapper } from "./controllerWrapper.js";
+export { default as errorHandler } from "./errorHandler.js";
