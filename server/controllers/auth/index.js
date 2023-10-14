2a926b9a9be95461bca6e7e38341f77930cc1272
Nataliya Kim <magistr1982@gmail.com>
2023-10-14 15:09:54 +0300

    Add google auth

--- server/controllers/auth/index.js
+++ server/controllers/auth/index.js
@@ -1,2 +1,3 @@
 export { default as signup } from "./signup.js"
 export { default as signin } from "./signin.js";
+export { default as google } from "./google.js";
--- server/routes/auth-router.js
+++ server/routes/auth-router.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { signin, signup } from "../controllers/auth/index.js";
+import { signin, signup, google } from "../controllers/auth/index.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/signup", signup)
 authRouter.post("/signin", signin);
+authRouter.post("/google", google);
 
 export default authRouter;
