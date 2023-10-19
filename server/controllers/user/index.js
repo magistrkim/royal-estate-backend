fcdbc35f46c2331d3da953f28ccc238d457bca12
Nataliya Kim <magistr1982@gmail.com>
2023-10-19 12:08:22 +0300

    Add get user listings

--- server/controllers/user/index.js
+++ server/controllers/user/index.js
@@ -1,2 +1,3 @@
 export { default as updateUser } from "./updateUser.js";
 export { default as deleteUser } from "./deleteUser.js";
+export { default as getUserListings } from "./getUserListings.js";
--- server/routes/user-router.js
+++ server/routes/user-router.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { updateUser, deleteUser } from "../controllers/user/index.js";
+import { updateUser, deleteUser, getUserListings } from "../controllers/user/index.js";
 import { verifyUser } from "../helpers/index.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/update/:id", verifyUser, updateUser);
 userRouter.delete("/delete/:id", verifyUser, deleteUser);
+userRouter.get("/listings/:id", verifyUser, getUserListings);
 
 export default userRouter;
