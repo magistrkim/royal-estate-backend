b3746278f1f087d9db53b4a0ca09b72a5a116f3c
Nataliya Kim <magistr1982@gmail.com>
2023-10-23 14:52:24 +0300

    create search api route

--- server/controllers/listing/index.js
+++ server/controllers/listing/index.js
@@ -2,3 +2,4 @@ export { default as createListing } from "./createListing.js";
 export { default as deleteListing } from "./deleteListing.js";
 export { default as updateListing } from "./updateListing.js";
 export { default as getListing } from "./getListing.js";
+export { default as getListings } from "./getListings.js";
--- server/routes/listing-router.js
+++ server/routes/listing-router.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createListing, deleteListing, updateListing, getListing } from "../controllers/listing/index.js";
+import { createListing, deleteListing, updateListing, getListing, getListings } from "../controllers/listing/index.js";
 import { verifyUser } from "../helpers/index.js";
 
 const listingRouter = express.Router();
@@ -8,5 +8,6 @@ listingRouter.post("/create", verifyUser, createListing);
 listingRouter.delete("/delete/:id", verifyUser, deleteListing);
 listingRouter.post("/update/:id", verifyUser, updateListing);
 listingRouter.get("/get/:id", getListing);
+listingRouter.get("/get", getListings);
 
 export default listingRouter;
