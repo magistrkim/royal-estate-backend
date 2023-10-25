f9448f7578da57958129b94d3fdb78071786424d
Nataliya Kim <magistr1982@gmail.com>
2023-10-25 17:03:16 +0300

    Add pre deploy settings

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import authRouter from "./routes/auth-router.js";
 import userRouter from "./routes/user-router.js";
 import listingRouter from "./routes/listing-router.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -17,6 +18,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -31,6 +34,11 @@ app.listen(3000, () => {
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
